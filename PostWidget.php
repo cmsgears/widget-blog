3223c62b99187511ae5f3d442b8ac42b4ccdd905
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-05-22 17:58:18 +0530

    Applied quick fixes.

---
+++ PostWidget.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+namespace cmsgears\widgets\blog;
+
+// Yii Imports
+use Yii;
+
+// CMG Imports
+use cmsgears\cms\common\config\CmsGlobal;
+
+use cmsgears\widgets\blog\base\PageWidget;
+
+/**
+ * PostWidget shows the most recent posts published on site.
+ *
+ * @since 1.0.0
+ */
+class PostWidget extends PageWidget {
+
+	// Variables ---------------------------------------------------
+
+	// Globals -------------------------------
+
+	// Constants --------------
+
+	// Public -----------------
+
+	// Protected --------------
+
+	// Variables -----------------------------
+
+	// Public -----------------
+
+	public $route = 'blog/search';
+
+	public $allPath = 'blog';
+
+	public $singlePath = 'blog';
+
+	// Protected --------------
+
+	protected $type = CmsGlobal::TYPE_POST;
+
+	protected $searchContent	= true;
+	protected $searchCategory	= true;
+	protected $searchtag		= true;
+
+	// Private ----------------
+
+	// Traits ------------------------------------------------------
+
+	// Constructor and Initialisation ------------------------------
+
+	public function init() {
+
+		$this->modelService = Yii::$app->factory->get( 'postService' );
+
+		parent::init();
+	}
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii parent classes --------------------
+
+	// yii\base\Widget --------
+
+	// CMG interfaces ------------------------
+
+	// CMG parent classes --------------------
+
+	// PostWidget ----------------------------
+
+}
